5c937f9345111354812ee971e68301881d3c54a3
Andrei Duque <91234632+andreiduque@users.noreply.github.com>
2021-11-16 13:27:25 -0300

    Fix switch to symbiosis (#2)

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,31 @@ import * as express from "express";
 import { connect } from "v1/config/database";
 import "reflect-metadata";
 import { setV1Controller } from "v1/v1.controller";
+import { Connection } from "@techmmunity/symbiosis-mongodb";
 
 const PORT = 4000;
 
 const server = async () => {
-	if (process.env.NODE_ENV !== "production") {
-		const { config } = await import("dotenv");
-		config();
+	let connection: Connection | undefined;
+
+	try {
+		if (process.env.NODE_ENV !== "production") {
+			const { config } = await import("dotenv");
+			config();
+		}
+
+		const app = express();
+
+		connection = await connect();
+
+		setV1Controller(app);
+
+		app.listen(PORT, process.env.HOST!);
+	} catch (err: any) {
+		if (connection) {
+			await connection.close();
+		}
 	}
-
-	const index = express();
-
-	await connect();
-
-	setV1Controller(index);
-
-	index.listen(PORT, process.env.HOST!);
 };
 
 server();
--- src/v1/v1.controller.ts
+++ src/v1/v1.controller.ts
@@ -1,7 +1,4 @@
 import { Router } from "express";
-import { setExampleController } from "./api/example/example.controller";
 import { API_VERSION } from "./config";
 
-export const setV1Controller = (mainRouter: Router) => {
-	setExampleController(mainRouter, API_VERSION);
-};
+export const setV1Controller = (mainRouter: Router) => {};
